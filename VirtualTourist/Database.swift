917c6358a04c24798b75c9e2c5b32ce9805b5f9d
Isabella Bencardino <isabella@bencardino.com.br>
2022-06-24 18:06:43 +0100

    show photo album screen

--- VirtualTourist/Database.swift
+++ VirtualTourist/Database.swift
@@ -4,7 +4,7 @@ protocol Database {
 
     func getImage(at path: String) -> Data?
     func createPin(latitude: Double, longitude: Double)
-    func createImage(for album: Album, blob: Data, url: String, id: String)
+    func createImage(for album: Album, blob: Data, url: String, id: Int)
     func changeStatus(of album: Album, to status: PhotoAlbumStatus)
     var pins: [Pin]? { get }
     var images: [Image]? { get set }
